fcd651daaabc4fdf8295248c47186cdd7206d132
Georgios Karnas <georgios@uber.com>
2018-07-06 14:37:46 -0700

    publish-docs script + update website + update ocular

---
+++ publish-docs.sh
@@ -0,0 +1,22 @@
+set -ex
+
+cd website
+yarn
+yarn build
+
+mv dist /tmp/nebula-docs
+
+cd ..
+git checkout gh-pages
+
+for F in /tmp/nebula-docs/*; do
+cp -R $F .
+git add $(basename "$F")
+done
+
+rm -rf /tmp/nebula-docs
+
+git commit --amend --no-verify -m "DOCS"
+
+git push origin gh-pages -f
+

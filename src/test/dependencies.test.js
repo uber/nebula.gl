67de2a326e06a0a8dc9535cd2be17863e97e4f40
Georgios Karnas <georgios@uber.com>
2018-07-24 23:03:59 -0700

    Add dependencies.test.js to check yarn.lock

---
+++ src/test/dependencies.test.js
@@ -0,0 +1,15 @@
+import { readFileSync } from 'fs';
+
+const RESOLVED_REGEX = /^resolved/gi;
+const ALLOWED_REPO = 'https://registry.yarnpkg.com/';
+
+it(`Ensure yarn.lock only uses ${ALLOWED_REPO}`, () => {
+  const lockfile = `${__dirname}/../../yarn.lock`;
+  const contents = readFileSync(lockfile, 'utf8').split('\n');
+
+  contents.forEach(line => {
+    if (line.trim().match(RESOLVED_REGEX)) {
+      expect(line).toEqual(expect.stringContaining(ALLOWED_REPO));
+    }
+  });
+});

ea6de19f398f63950907a69207775147bfd2635e
Clay Anderson <supersonicclay@gmail.com>
2018-10-07 20:11:57 -0600

    Factor event types into separate file

---
+++ modules/core/src/lib/event-types.js
@@ -0,0 +1,46 @@
+// @flow
+
+import type { Position } from '../geojson-types.js';
+
+export type DeckGLPick = {
+  index: number,
+  object: any,
+  isEditingHandle: ?boolean
+};
+
+export type ClickEvent = {
+  picks: DeckGLPick[],
+  screenCoords: Position,
+  groundCoords: Position
+};
+
+export type DoubleClickEvent = {
+  groundCoords: Position
+};
+
+export type StartDraggingEvent = {
+  picks: DeckGLPick[],
+  screenCoords: Position,
+  groundCoords: Position,
+  dragStartScreenCoords: Position,
+  dragStartGroundCoords: Position
+};
+
+export type StopDraggingEvent = {
+  picks: DeckGLPick[],
+  screenCoords: Position,
+  groundCoords: Position,
+  dragStartScreenCoords: Position,
+  dragStartGroundCoords: Position
+};
+
+export type PointerMoveEvent = {
+  screenCoords: Position,
+  groundCoords: Position,
+  picks: DeckGLPick[],
+  isDragging: boolean,
+  dragStartPicks: ?(DeckGLPick[]),
+  dragStartScreenCoords: ?Position,
+  dragStartGroundCoords: ?Position,
+  sourceEvent: any
+};

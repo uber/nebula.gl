4c645fa40ad86cb9d9a7a0d63f404890e0a45ce9
Ib Green <ib@uber.com>
2019-03-20 14:52:19 -0700

    Initial gatsby website structure

---
+++ website/src/components/example-list.jsx
@@ -0,0 +1,15 @@
+import {
+  BasicOverlayExample,
+  ClusteringOverlayExample,
+  EditPointsExample,
+  EditPolygonsExample,
+  WorldHeritageExample
+} from '../../../docs/examples/examples';
+
+export default {
+  BasicOverlayExample: {app: BasicOverlayExample, path: 'examples/'},
+  ClusteringOverlayExample: {app: ClusteringOverlayExample, path: 'examples/'},
+  EditPointsExample: {app: EditPointsExample, path: 'examples/'},
+  EditPolygonsExample: {app: EditPolygonsExample, path: 'examples/'},
+  WorldHeritageExample: {app: WorldHeritageExample, path: 'examples/'},
+};

1b6f4d5bfe9431bac2b899dd6c20dc39f0a51ac8
Georgios Karnas <georgios@uber.com>
2019-04-21 10:35:52 -0700

    no-map example

---
+++ examples/no-map/app.js
@@ -0,0 +1,15 @@
+// @flow
+import document from 'global/document';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+import ExampleNoMap from './example-no-map';
+
+const root = document.createElement('div');
+
+if (document.body) {
+  document.body.style.margin = '0';
+
+  document.body.appendChild(root);
+  ReactDOM.render(<ExampleNoMap />, root);
+}
---
+++ examples/no-map/example-no-map.js
@@ -0,0 +1,37 @@
+// @flow
+import { BitmapLayer } from '@deck.gl/layers';
+import Example from '../deck/example';
+
+// https://en.wikipedia.org/wiki/Wikipedia:Featured_pictures/Animals/Birds#/media/File:Dendrocygna_eytoni_-_Macquarie_University.jpg
+// JJ Harrison (jjharrison89@facebook.com) - Own work - CC BY-SA 4.0
+const IMAGE =
+  'https://upload.wikimedia.org/wikipedia/commons/1/1b/Dendrocygna_eytoni_-_Macquarie_University.jpg';
+
+export default class ExampleNoMap extends Example {
+  constructor() {
+    super();
+    this.state.viewport = {
+      bearing: 0,
+      height: 0,
+      latitude: 0,
+      longitude: 0,
+      pitch: 0,
+      width: 0,
+      zoom: 5
+    };
+  }
+
+  renderStaticMap(viewport: Object) {
+    return null;
+  }
+
+  customizeLayers(layers: Object[]) {
+    layers.unshift(
+      new BitmapLayer({
+        id: 'bitmap-layer',
+        bounds: [-10, -10, +10, +10],
+        image: IMAGE
+      })
+    );
+  }
+}

97076109f9ac8f9184181c6b43a9365d669f6c24
Clay Anderson <supersonicclay@gmail.com>
2019-05-08 15:55:04 -0600

    Add core module

--- examples/webpack.config.local.js
+++ examples/webpack.config.local.js
@@ -11,6 +11,7 @@ const webpack = require('webpack');
 
 const LIB_DIR = resolve(__dirname, '..');
 const MAIN_SRC_DIR = resolve(LIB_DIR, './modules/main/src');
+const CORE_SRC_DIR = resolve(LIB_DIR, './modules/core/src');
 const LAYERS_SRC_DIR = resolve(LIB_DIR, './modules/layers/src');
 const OVERLAYS_SRC_DIR = resolve(LIB_DIR, './modules/overlays/src');
 const REACT_EDITOR_LITE_SRC_DIR = resolve(LIB_DIR, './modules/react-map-gl-draw/src');
@@ -33,6 +34,9 @@ function makeLocalDevConfig(EXAMPLE_DIR = LIB_DIR) {
       alias: {
         'nebula.gl/dist': MAIN_SRC_DIR,
         'nebula.gl': MAIN_SRC_DIR,
+
+        '@nebula.gl/core/dist': CORE_SRC_DIR,
+        '@nebula.gl/core': CORE_SRC_DIR,
 
         '@nebula.gl/layers/dist': LAYERS_SRC_DIR,
         '@nebula.gl/layers': LAYERS_SRC_DIR,

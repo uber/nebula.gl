7952d53d82437d6bdf49daa5d21743286df1f552
Xintong Xia <xintong@uber.com>
2019-06-24 10:38:52 -0700

    Support polygon closing styling
    resolve comments

--- modules/react-map-gl-draw/src/style.js
+++ modules/react-map-gl-draw/src/style.js
@@ -7,12 +7,19 @@ const rectStyle = {
   width: 12
 };
 
+const closingRectStyle = {
+  x: -10,
+  y: -10,
+  height: 20,
+  width: 20
+};
+
 export function getFeatureStyle({ feature, state }) {
   const style = {
     stroke:
       state === RENDER_STATE.SELECTED || state === RENDER_STATE.HOVERED
         ? '#7ac943'
-        : state === RENDER_STATE.UNCOMMITTED
+        : state === RENDER_STATE.UNCOMMITTED || state === RENDER_STATE.CLOSING
           ? '#a9a9a9'
           : '#000',
     strokeWidth: 4,
@@ -23,7 +30,7 @@ export function getFeatureStyle({ feature, state }) {
           ? '#7ac943'
           : state === RENDER_STATE.SELECTED
             ? '#ffff00'
-            : state === RENDER_STATE.UNCOMMITTED
+            : state === RENDER_STATE.UNCOMMITTED || state === RENDER_STATE.CLOSING
               ? '#a9a9a9'
               : '#000000',
     fillOpacity:
@@ -33,7 +40,7 @@ export function getFeatureStyle({ feature, state }) {
           ? 0.7
           : state === RENDER_STATE.HOVERED
             ? 0.5
-            : state === RENDER_STATE.UNCOMMITTED
+            : state === RENDER_STATE.UNCOMMITTED || state === RENDER_STATE.CLOSING
               ? 0.3
               : 1
   };
@@ -46,6 +53,11 @@ export function getFeatureStyle({ feature, state }) {
       break;
     case RENDER_TYPE.LINE_STRING:
       style.fill = 'none';
+      break;
+    case RENDER_TYPE.POLYGON:
+      if (state === RENDER_STATE.CLOSING) {
+        style.strokeDasharray = '4,2';
+      }
       break;
     case RENDER_TYPE.RECTANGLE:
       if (state === RENDER_STATE.UNCOMMITTED) {
@@ -63,7 +75,7 @@ export function getEditHandleStyle({ feature, index, state }) {
     fill:
       state === RENDER_STATE.INACTIVE
         ? '#ffffff'
-        : state === RENDER_STATE.SELECTED
+        : state === RENDER_STATE.SELECTED || state === RENDER_STATE.CLOSING
           ? '#ffff00'
           : state === RENDER_STATE.HOVERED
             ? '#7ac943'
@@ -75,13 +87,18 @@ export function getEditHandleStyle({ feature, index, state }) {
         ? 1
         : state === RENDER_STATE.HOVERED
           ? 1
-          : state === RENDER_STATE.SELECTED
+          : state === RENDER_STATE.SELECTED || state === RENDER_STATE.CLOSING
             ? 0.8
             : state === RENDER_STATE.UNCOMMITTED
               ? 0.3
               : 1,
-    stroke: state === RENDER_STATE.SELECTED || state === RENDER_STATE.HOVERED ? '#7ac943' : '#000',
-    strokeWidth: state === RENDER_STATE.SELECTED ? 4 : 2,
+    stroke:
+      state === RENDER_STATE.SELECTED ||
+      state === RENDER_STATE.HOVERED ||
+      state === RENDER_STATE.CLOSING
+        ? '#7ac943'
+        : '#000',
+    strokeWidth: state === RENDER_STATE.SELECTED || state === RENDER_STATE.CLOSING ? 4 : 2,
     r: 8
   };
 
@@ -104,7 +121,11 @@ export function getEditHandleStyle({ feature, index, state }) {
     case RENDER_TYPE.LINE_STRING:
     case RENDER_TYPE.POLYGON:
     case RENDER_TYPE.RECTANGLE:
-      Object.assign(style, rectStyle);
+      if (state === RENDER_STATE.CLOSING) {
+        Object.assign(style, closingRectStyle);
+      } else {
+        Object.assign(style, rectStyle);
+      }
       break;
     default:
   }

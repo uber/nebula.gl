5c3dba6d352061a7a6da148f6ed1035d1881615f
Clay Anderson <claya@uber.com>
2019-08-22 14:48:43 -0600

    Tweak script

--- scripts/flow-copy-src.js
+++ scripts/flow-copy-src.js
@@ -14,11 +14,10 @@ const { lstatSync, readdirSync } = require('fs');
 const { join } = require('path');
 const flowCopySource = require('flow-copy-source');
 
-const isDirectory = source => lstatSync(source).isDirectory();
-const getDirectories = source =>
-  readdirSync(source)
-    .map(name => join(source, name))
-    .filter(isDirectory);
+const getDirectories = directory =>
+  readdirSync(directory)
+    .map(name => join(directory, name))
+    .filter(entry => lstatSync(entry).isDirectory());
 
 async function run() {
   const directories = getDirectories(path.resolve(__dirname, '../modules'));

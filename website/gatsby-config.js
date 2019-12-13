4f1ca11adb291d9e96ecd135148d3be8c3a59650
Georgios Karnas <georgios@uber.com>
2019-12-13 15:46:11 -0800

    fix website/gatsby-config

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,25 +1,24 @@
 const ocularConfig = require('./ocular-config');
 
-// const remark = gatsbyConfig.plugins.find(p => p.resolve === 'gatsby-transformer-remark');
-// // If you are using gatsby-remark-responsive-iframe, it must appear after gatsby-remark-embedded-codesandbox
-// // plugin in your configuration or the iframe will not be transformed.
+// If you are using gatsby-remark-responsive-iframe, it must appear after gatsby-remark-embedded-codesandbox
+// plugin in your configuration or the iframe will not be transformed.
 
-// remark.options.plugins.unshift({
-//   resolve: 'gatsby-remark-embedded-codesandbox',
-//   options: {
-//     directory: `${__dirname}/../examples/codesandbox`,
-//     protocol: 'embedded-codesandbox://',
-//     embedOptions: {
-//       // https://codesandbox.io/docs/embedding#embed-options
-//       codemirror: 1,
-//       fontsize: 12,
-//       hidenavigation: 1,
-//       view: 'split'
-//     },
-//     getIframe: url =>
-//       `<iframe src="${url}" style="width: 70vw; height: 70vh;" class="embedded-codesandbox" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`
-//   }
-// });
+const embeddedCodesandbox = {
+  resolve: 'gatsby-remark-embedded-codesandbox',
+  options: {
+    directory: `${__dirname}/../examples/codesandbox`,
+    protocol: 'embedded-codesandbox://',
+    embedOptions: {
+      // https://codesandbox.io/docs/embedding#embed-options
+      codemirror: 1,
+      fontsize: 12,
+      hidenavigation: 1,
+      view: 'split'
+    },
+    getIframe: url =>
+      `<iframe src="${url}" style="width: 70vw; height: 70vh;" class="embedded-codesandbox" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`
+  }
+};
 
 module.exports = {
   plugins: [
@@ -29,6 +28,12 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-flow`
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [embeddedCodesandbox]
+      }
     }
   ]
 };

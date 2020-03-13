a6c603d6ab5310ef89a1084bab17bd2857281a64
Clay Anderson <469582+supersonicclay@users.noreply.github.com>
2020-03-13 11:20:12 -0600

    Add new editor module and example app (#349)
    * Adds a new `@nebula.gl/editor` module
    * Adds a super-basic `Toolbox` component just to show how props and callbacks can flow
    * Adds a new `editor` example so that we can experiment.

---
+++ examples/editor/example.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import DeckGL from '@deck.gl/react';
+import { EditableGeoJsonLayer } from '@nebula.gl/layers';
+import { Toolbox } from '@nebula.gl/editor';
+import { DrawPolygonMode } from '@nebula.gl/edit-modes';
+import { StaticMap } from 'react-map-gl';
+
+const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1IjoiZ2Vvcmdpb3MtdWJlciIsImEiOiJjanZidTZzczAwajMxNGVwOGZrd2E5NG90In0.gdsRu_UeU_uPi9IulBruXA';
+
+const initialViewState = {
+  longitude: -122.43,
+  latitude: 37.775,
+  zoom: 12
+};
+
+export function Example() {
+  const [features, setFeatures] = React.useState({
+    type: 'FeatureCollection',
+    features: []
+  });
+  const [selectedFeatureIndexes] = React.useState([]);
+  const [mode, setMode] = React.useState(() => DrawPolygonMode);
+  const [modeConfig, setModeConfig] = React.useState(null);
+
+  const layer = new EditableGeoJsonLayer({
+    data: features,
+    mode,
+    modeConfig,
+    selectedFeatureIndexes,
+
+    onEdit: ({ updatedData }) => {
+      setFeatures(updatedData);
+    }
+  });
+
+  return (
+    <>
+      <DeckGL
+        initialViewState={initialViewState}
+        controller={{
+          doubleClickZoom: false
+        }}
+        layers={[layer]}
+        getCursor={layer.getCursor.bind(layer)}
+      >
+        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+      </DeckGL>
+      <Toolbox
+        position="topright"
+        mode={mode}
+        onSetMode={setMode}
+        modeConfig={modeConfig}
+        onSetModeConfig={setModeConfig}
+      />
+    </>
+  );
+}
---
+++ modules/editor/src/toolbox.js
@@ -0,0 +1,57 @@
+// @flow
+
+import React from 'react';
+import { DrawPointMode, DrawPolygonMode } from '@nebula.gl/edit-modes';
+
+type ContainerPosition = 'topright' | 'bottomright' | 'bottomleft' | 'topleft';
+
+export type Props = {
+  mode: any,
+  modeConfig: any,
+  onSetMode: any => mixed,
+  onSetModeConfig: any => mixed,
+  position?: ?ContainerPosition
+};
+
+const MODE_BUTTONS = [
+  // TODO: change these to icons
+  { mode: DrawPointMode, content: <span>Draw point</span> },
+  { mode: DrawPolygonMode, content: <span>Draw polygon</span> }
+];
+
+export function Toolbox({ mode, modeConfig, onSetMode, onSetModeConfig, position }: Props) {
+  return (
+    <div style={getContainerStyles(position)}>
+      {MODE_BUTTONS.map((modeButton, i) => (
+        <button
+          key={i}
+          style={{ backgroundColor: mode === modeButton.mode ? 'red' : 'gray' }}
+          onClick={() => onSetMode(() => modeButton.mode)}
+        >
+          {modeButton.content}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function getContainerStyles(position: ?ContainerPosition) {
+  let styles = {
+    position: 'absolute'
+  };
+
+  switch (position) {
+    case 'topright':
+    default:
+      styles = {
+        ...styles,
+        top: '10px',
+        right: '10px',
+        display: 'flex',
+        flexFlow: 'column'
+      };
+      break;
+  }
+
+  return styles;
+}

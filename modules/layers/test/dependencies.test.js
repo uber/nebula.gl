c351c497f84e222371d7462d07fe1af682dd7e60
Clay Anderson <supersonicclay@gmail.com>
2020-03-17 11:36:48 -0600

    Check yarn.lock in examples also

--- modules/layers/test/dependencies.test.js
+++ modules/layers/test/dependencies.test.js
@@ -1,15 +1,22 @@
-import { readFileSync } from 'fs';
+import { readFileSync, readdirSync, existsSync } from 'fs';
 
 const RESOLVED_REGEX = /^resolved/gi;
-const ALLOWED_REPO = 'https://registry.yarnpkg.com/';
+const NOT_ALLOWED_REPO = 'unpm.u';
+const EXAMPLES_DIR = `${__dirname}/../../../examples`;
 
-it(`Ensure yarn.lock only uses ${ALLOWED_REPO}`, () => {
-  const lockfile = `${__dirname}/../../../yarn.lock`;
-  const contents = readFileSync(lockfile, 'utf8').split('\n');
+it(`Ensure yarn.lock doesn't contain ${NOT_ALLOWED_REPO}`, () => {
+  const rootLockFile = `${__dirname}/../../../yarn.lock`;
+  const exampleLockFiles = readdirSync(`${EXAMPLES_DIR}`)
+    .map(example => `${EXAMPLES_DIR}/${example}/yarn.lock`)
+    .filter(existsSync);
 
-  contents.forEach(line => {
-    if (line.trim().match(RESOLVED_REGEX)) {
-      expect(line).toEqual(expect.stringContaining(ALLOWED_REPO));
-    }
+  [rootLockFile, ...exampleLockFiles].forEach(lockfile => {
+    const contents = readFileSync(lockfile, 'utf8').split('\n');
+
+    contents.forEach(line => {
+      if (line.trim().match(RESOLVED_REGEX)) {
+        expect(line).not.toEqual(expect.stringContaining(NOT_ALLOWED_REPO));
+      }
+    });
   });
 });

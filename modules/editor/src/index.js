e83135c8e1940b3d61bce9c9be5f95ddea851b3a
Tyler Prete <tprete@uber.com>
2020-03-17 15:53:27 -0600

    Add exported types and functions for import/export to index.js.

--- modules/editor/src/index.js
+++ modules/editor/src/index.js
@@ -2,3 +2,5 @@
 
 export { Toolbox } from './toolbox.js';
 export { ImportModal } from './import-modal.js';
+export { ImportData, ValidImportData, InvalidImportData, parseImport } from './lib/importer.js';
+export { ExportParameters, toKml, toWkt, toGeoJson, toStats, toIds } from './lib/exporter.js';

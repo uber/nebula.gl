fbc58c8fd077a4b23f2ce864b202e81be5feb013
Tyler Prete <tprete@uber.com>
2020-03-16 18:18:10 -0600

    Add tests for importer functionality.

--- modules/editor/src/utils.js
+++ modules/editor/src/utils.js
@@ -1,9 +1,17 @@
 // @flow
 import uuid from 'uuid/v4';
-// import type { GeoJsonFeature } from './types.js';
+import type { GeoJsonFeature } from './types.js';
 
 export const NEW_PREFIX = '__NEW__';
 
 export function newFeatureId(): string {
   return `${NEW_PREFIX}${uuid()}`;
+}
+
+export function isFeatureNew(feature: GeoJsonFeature): boolean {
+  return isFeatureIdNew(feature.id);
+}
+
+export function isFeatureIdNew(id: string): boolean {
+  return id.startsWith(NEW_PREFIX);
 }

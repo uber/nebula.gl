8ab478e43708019e4795bb2957c7a492beb788b6
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-03-23 16:22:12 -0400

    Prettier

--- modules/edit-modes/src/lib/draw-polygon-by-dragging-mode.js
+++ modules/edit-modes/src/lib/draw-polygon-by-dragging-mode.js
@@ -6,41 +6,41 @@ import { getPickedEditHandle } from '../utils.js';
 import { DrawPolygonMode } from './draw-polygon-mode';
 
 export class DrawPolygonByDraggingMode extends DrawPolygonMode {
-    handleClick() {
-        // No-op
+  handleClick() {
+    // No-op
+  }
+
+  handleStartDragging(event: ClickEvent) {
+    event.cancelPan();
+  }
+
+  handleStopDragging(event: ClickEvent, props: ModeProps<FeatureCollection>) {
+    this.addClickSequence(event);
+    const clickSequence = this.getClickSequence();
+
+    if (clickSequence.length > 2) {
+      // Complete the polygon.
+      const polygonToAdd: Polygon = {
+        type: 'Polygon',
+        coordinates: [[...clickSequence, clickSequence[0]]]
+      };
+
+      this.resetClickSequence();
+
+      const editAction = this.getAddFeatureOrBooleanPolygonAction(polygonToAdd, props);
+      if (editAction) {
+        props.onEdit(editAction);
+      }
     }
+  }
 
-    handleStartDragging(event: ClickEvent) {
-        event.cancelPan();
+  handleDragging(event: ClickEvent) {
+    const { picks } = event;
+    const clickedEditHandle = getPickedEditHandle(picks);
+
+    if (!clickedEditHandle) {
+      // Don't add another point right next to an existing one.
+      this.addClickSequence(event);
     }
-
-    handleStopDragging(event: ClickEvent, props: ModeProps<FeatureCollection>) {
-        this.addClickSequence(event);
-        const clickSequence = this.getClickSequence();
-
-        if (clickSequence.length > 2) {
-            // Complete the polygon.
-            const polygonToAdd: Polygon = {
-                type: 'Polygon',
-                coordinates: [[...clickSequence, clickSequence[0]]]
-            };
-
-            this.resetClickSequence();
-
-            const editAction = this.getAddFeatureOrBooleanPolygonAction(polygonToAdd, props);
-            if (editAction) {
-                props.onEdit(editAction);
-            }
-        }
-    }
-
-    handleDragging(event: ClickEvent, props: ModeProps<FeatureCollection>) {
-        const { picks } = event;
-        const clickedEditHandle = getPickedEditHandle(picks);
-
-        if (!clickedEditHandle) {
-            // Don't add another point right next to an existing one.
-            this.addClickSequence(event);
-        }
-    }
+  }
 }

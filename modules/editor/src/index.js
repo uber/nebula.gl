5f18f5c32b773ded660a1335eeae1aed5fbc82b4
Tyler Prete <tprete@uber.com>
2020-03-24 15:05:13 -0600

    Removing module exports of importer & exporter.
    For now we'll just export the modals and components.

--- modules/editor/src/index.js
+++ modules/editor/src/index.js
@@ -4,7 +4,3 @@ export { Toolbox } from './toolbox.js';
 export { ExportModal } from './export-modal.js';
 export { ImportModal } from './import-modal.js';
 export { ImportComponent } from './import-component.js';
-export type { ImportData, ValidImportData, InvalidImportData } from './lib/importer.js';
-export { parseImport } from './lib/importer.js';
-export type { ExportParameters } from './lib/exporter.js';
-export { toKml, toWkt, toGeoJson, toStats } from './lib/exporter.js';

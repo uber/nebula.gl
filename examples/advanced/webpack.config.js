5dd11660a8dc4261a604fc85a1d82434e461b667
kikonejacob <kikonejacob@gmail.com>
2020-04-04 21:49:55 -0500

    attempt to fix advanced sample

--- examples/advanced/webpack.config.js
+++ examples/advanced/webpack.config.js
@@ -28,7 +28,7 @@ const CONFIG = {
             presets: [
               require('@babel/preset-env'),
               require('@babel/preset-react'),
-              require('@babel/preset-flow'),
+              require('@babel/preset-typescript'),
             ],
             plugins: [
               require('@babel/plugin-proposal-class-properties'),

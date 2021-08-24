72f0b3457646bccc5f5a906bf27a81a321e43854
Alexander CherryTea <Akiyamka@gmail.com>
2021-08-24 23:42:48 +0300

    fix: "this.state is null" error (#628)

--- modules/layers/src/layers/editable-geojson-layer.ts
+++ modules/layers/src/layers/editable-geojson-layer.ts
@@ -476,6 +476,11 @@ export default class EditableGeoJsonLayer extends EditableLayer {
   }
 
   getCursor({ isDragging }: { isDragging: boolean }) {
+    if (this.state === null) {
+      // Layer in 'Awaiting state'
+      return;
+    };
+    
     let { cursor } = this.state;
     if (!cursor) {
       // default cursor
